c0c6a15a6c7597d22e4c677d26fb0e34e67b61c5
JMarioBorrayo <60480292+JMarioBorrayo@users.noreply.github.com>
2022-05-24 13:31:21 -0600

    nuevo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera } from '@awesome-cordova-plugins/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  imgURL;
+  constructor(private camera: Camera) {
 
-  constructor() {}
+  }
+
+
+
+  getCamera(){
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.FILE_URI
+    }).then((res)=>{
+      this.imgURL = res
+    }).catch(e =>{
+      console.log(e)
+    }) 
+    
+  
+  }
+
+  getGallery(){
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then((res)=>{
+      this.imgURL = 'data:image/jpeg;base64,' + res
+    }).catch(e =>{
+      console.log(e)
+    }) 
+    
+  
+  }
 
 }
